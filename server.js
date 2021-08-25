8453952df9509f4e7dfd284d5f92ee21123ba7c2
M-Minkov <MinkoMinkov@outlook.co.nz>
2021-08-26 05:25:51 +1200

    Multer file saving, and returning file info
    Saves the file into an uploads folder, and returns the main header information of the file

--- server.js
+++ server.js
@@ -1,15 +1,43 @@
 var express = require('express');
 var cors = require('cors');
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 require('dotenv').config()
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.urlencoded({extended : true}));
+app.use(express.json());
+
+
+
 
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
+
+
+
+function recieveFile(req, res) {
+  try {
+    let name_of_file = req.file.originalname;
+    let type_of_file = req.file.mimetype;
+    let size_of_file = req.file.size;
+
+    res.json({
+      name: name_of_file,
+      type: type_of_file,
+      size: size_of_file
+    })
+  }
+  catch {
+    res.status(400).json({"error":"error"});
+  }
+}
+
+app.post("/api/fileanalyse", upload.single("upfile"), recieveFile);
 
 
 
